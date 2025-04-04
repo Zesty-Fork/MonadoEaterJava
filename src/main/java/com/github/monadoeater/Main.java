cd0f7fff28596c3293f65374d3ceec72270ceb5e
Nathan James Jones <nathan.jones@ivanhoeelectric.com>
2025-04-04 13:00:43 -0700

    Added logic for reading HTML and deciphering from SQLite

--- src/main/java/com/github/monadoeater/Main.java
+++ src/main/java/com/github/monadoeater/Main.java
@@ -2,25 +2,38 @@ package com.github.monadoeater;
 import com.github.monadoeater.db.sqlite.EaterDb;
 import com.github.monadoeater.website.fandom.Webpage;
 import com.github.monadoeater.website.fandom.WebsiteCrawler;
+import org.jsoup.nodes.Document;
 
+import java.nio.charset.StandardCharsets;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Blob;
 
 public class Main {
     private static final String FANDOM_NAME = "xenoblade";
 
     // Scrape fandom website and load into eater database.
     private static void eatFandomWebsite(String fandomName) {
-        EaterDb.connect();
-        WebsiteCrawler websiteCrawler = new WebsiteCrawler(fandomName, "Brams_Driver");
+        WebsiteCrawler websiteCrawler = new WebsiteCrawler(fandomName, "Z∞");
         Webpage webpage = websiteCrawler.crawl();
         while (webpage != null) {
             System.out.println("Eating: ".concat(webpage.getUrl()));
             webpage = websiteCrawler.crawl();
-            EaterDb.insertWebpage(webpage);
+            if (webpage != null) {
+                EaterDb.insertWebpage(webpage);
+            }
         }
+    }
+
+    private static void printHtmlText() {
+        Document document = EaterDb.selectHtml(3);
+        System.out.println(document.text());
     }
 
 
     public static void main(String[] args) {
-        eatFandomWebsite(FANDOM_NAME);
+        EaterDb.connect();
+        // eatFandomWebsite(FANDOM_NAME);
+        printHtmlText();
     }
 }
--- src/main/java/com/github/monadoeater/db/sqlite/EaterDb.java
+++ src/main/java/com/github/monadoeater/db/sqlite/EaterDb.java
@@ -6,10 +6,10 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
+import org.jsoup.nodes.Document;
+import org.jsoup.Jsoup;
+
 
 
 public class EaterDb {
@@ -35,13 +35,39 @@ public class EaterDb {
         String sqliteUrl = "jdbc:sqlite:" + DB_PATH;
         String sqlQuery = "INSERT INTO webpage (url, html) VALUES (?, ?);";
         try (Connection conn = DriverManager.getConnection(sqliteUrl)) {
-            PreparedStatement stmt = conn.prepareStatement(sqlQuery);
-            stmt.setString(1, webpage.getUrl());
-            stmt.setBytes(2, webpage.getDocument().html().getBytes());
-            stmt.execute();
+            PreparedStatement statement = conn.prepareStatement(sqlQuery);
+            statement.setString(1, webpage.getUrl());
+            statement.setBytes(2, webpage.getDocument().html().getBytes());
+            statement.execute();
         } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
     }
 
+    // Insert webpage URL and binary data to SQLite database.
+    public static Document selectHtml(int id) {
+        String sqliteUrl = "jdbc:sqlite:" + DB_PATH;
+        byte[] htmlBlob = null;
+        String sqlQuery = "SELECT html FROM webpage WHERE id = ?;";
+        try (Connection conn = DriverManager.getConnection(sqliteUrl)) {
+            PreparedStatement statement = conn.prepareStatement(sqlQuery);
+            statement.setInt(1, id);
+            ResultSet resultSet = statement.executeQuery();
+            htmlBlob = resultSet.getBytes("html");
+        }
+        catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return blobToJsoupDocument(htmlBlob);
+    }
+
+
+    private static Document blobToJsoupDocument(byte[] blobData) {
+        Document document = null;
+        if (blobData != null) {
+            String htmlContent = new String(blobData);
+            document = Jsoup.parse(htmlContent);
+        }
+        return document;
+    }
 }
