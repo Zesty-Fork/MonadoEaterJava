aea2eea8dbf29a33a52171670eb72743c339ad5d
Nathan James Jones <nathan.jones@ivanhoeelectric.com>
2025-05-08 09:13:50 -0700

    Removed unneeded libraries

--- src/main/java/com/github/monadoeater/Main.java
+++ src/main/java/com/github/monadoeater/Main.java
@@ -4,17 +4,13 @@ import com.github.monadoeater.website.fandom.Webpage;
 import com.github.monadoeater.website.fandom.WebsiteCrawler;
 import org.jsoup.nodes.Document;
 
-import java.nio.charset.StandardCharsets;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Blob;
 
 public class Main {
-    private static final String FANDOM_NAME = "xenoblade";
+    private static final String FANDOM_NAME = "xenosaga";
 
     // Scrape fandom website and load into eater database.
     private static void eatFandomWebsite(String fandomName) {
-        WebsiteCrawler websiteCrawler = new WebsiteCrawler(fandomName, "Z∞");
+        WebsiteCrawler websiteCrawler = new WebsiteCrawler(fandomName);
         Webpage webpage = websiteCrawler.crawl();
         while (webpage != null) {
             System.out.println("Eating: ".concat(webpage.getUrl()));
@@ -33,7 +29,7 @@ public class Main {
 
     public static void main(String[] args) {
         EaterDb.connect();
-        // eatFandomWebsite(FANDOM_NAME);
-        printHtmlText();
+        eatFandomWebsite(FANDOM_NAME);
+        //printHtmlText();
     }
 }
